bcd48a6e407e321496467629532cdec148a4e93f
Toast Kid <super_q1m@yahoo.co.jp>
2019-03-13 00:05:41 +0900

    refactor Delete unused constructor's parameter.

--- app/src/main/java/jp/toastkid/nishikie/libs/BitmapScaling.kt
+++ app/src/main/java/jp/toastkid/nishikie/libs/BitmapScaling.kt
@@ -1,13 +1,12 @@
 package jp.toastkid.nishikie.libs
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Matrix
 
 /**
  * @author toastkidjp
  */
-class BitmapScaling(private val context: Context) {
+class BitmapScaling {
 
     fun resizeImage(img: Bitmap, toSize: Float = MAXIMUM_BITMAP_SIZE): Bitmap {
         val size = img.byteCount
