60b346593277176ae1ab2ffe3e3cd7d2345de289
Toast Kid <super_q1m@yahoo.co.jp>
2019-03-13 00:09:13 +0900

    refactor Add comments in BitmapScaling.

--- app/src/main/java/jp/toastkid/nishikie/libs/BitmapScaling.kt
+++ app/src/main/java/jp/toastkid/nishikie/libs/BitmapScaling.kt
@@ -4,12 +4,23 @@ import android.graphics.Bitmap
 import android.graphics.Matrix
 
 /**
+ * Bitmap scale utility.
+ *
  * @author toastkidjp
  */
 object BitmapScaling {
 
+    /**
+     * Maximum bitmap size(byte length).
+     */
     private const val MAXIMUM_BITMAP_SIZE = 5000900f
 
+    /**
+     * Resize passed bitmap with specified size
+     * .
+     * @param img [Bitmap]
+     * @param toSize Default: MAXIMUM_BITMAP_SIZE
+     */
     fun resizeImage(img: Bitmap, toSize: Float = MAXIMUM_BITMAP_SIZE): Bitmap {
         val size = img.byteCount
         val scale = Math.sqrt((toSize / size.toFloat()).toDouble()).toFloat()
